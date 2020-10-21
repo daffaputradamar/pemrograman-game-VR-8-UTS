14f367aad0e09bcdb4c95825955ed31769ffd368
Daffa Akbar <daffaputradamar@gmail.com>
2020-10-21 10:31:05 +0700

    init commit

---
+++ Assets/Scripts/AmbilObjek.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmbilObjek : MonoBehaviour
+{
+    public GameObject bola;
+    public GameObject tangan;
+    public GameObject telapak;
+    public Text imgText;
+    bool tertangkap;
+    public Vector3 posisiBola;
+
+    private Coroutine currentCoroutine = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!tertangkap)
+            {
+                bola.transform.SetParent(tangan.transform);
+                bola.transform.localPosition = new Vector3(telapak.transform.localPosition.x + .15f, telapak.transform.localPosition.y - .2f, telapak.transform.localPosition.z + .6f);
+                bola.GetComponent<Renderer>().material.color = Color.blue;
+
+                imgText.text = "Bola ditangkap";
+                imgText.gameObject.SetActive(true);
+
+                if (currentCoroutine != null)
+                    StopCoroutine(currentCoroutine);
+                currentCoroutine = StartCoroutine(deactivateText());
+            }
+            else if (tertangkap)
+            {
+                bola.transform.SetParent(null);
+                bola.transform.localPosition = posisiBola;
+                bola.GetComponent<Renderer>().material.color = Color.red;
+
+                imgText.text = "Bola dilepas";
+                imgText.gameObject.SetActive(true);
+
+                if (currentCoroutine != null)
+                    StopCoroutine(currentCoroutine);
+                currentCoroutine = StartCoroutine(deactivateText());
+            }
+            tertangkap = !tertangkap;
+        }
+    }
+
+    IEnumerator deactivateText()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        imgText.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public float delta = 7f;  // Amount to move left and right from the start point
+    public float speed = 1f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
